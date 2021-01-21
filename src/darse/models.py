97105513dc977d310bd4fb3279fdec992c82cee5
AB Noori <ab@ABs-MacBook-Pro.local>
2021-01-21 01:07:11 -0800

    updatee/edit to app

--- src/darse/models.py
+++ src/darse/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+
+import datetime
+
+class Post(models.Model):
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    summary     = models.TextField(default="Wow this is Cool!")
+    # datetime    = datetime.now()
+    # timezone    = django.utils.timezone.now()
+   
+
